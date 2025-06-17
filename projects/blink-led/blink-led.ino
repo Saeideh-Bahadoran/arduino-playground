adf3dd91b0893352328323b5cc5e23226370a9e6
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2025-06-17 14:31:33 +0200

    day 2 - the second project 'blink-led'

--- projects/blink-led/blink-led.ino
+++ projects/blink-led/blink-led.ino
@@ -1,1 +1,15 @@
- 
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+
+
+}
+
+void loop() {
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  delay(1000);
+}
